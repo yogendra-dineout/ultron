2c5e368a8ea4bfca4fd86e3b5c4efa5d8f74c9e8
yogendra-dineout <yogendra.singh@dineout.co.in>
2019-03-24 00:08:43 +0530

    first commit

---
+++ src/test/java/dineout/ultron/UltronApplicationTests.java
@@ -0,0 +1,16 @@
+package dineout.ultron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UltronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
